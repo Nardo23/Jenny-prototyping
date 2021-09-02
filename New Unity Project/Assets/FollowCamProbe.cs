0270dc39c2924fe07a8f48051b7906e6f0d4e554
pee <leonardo.price@gmail.com>
2021-09-02 14:54:27 -0400

    project commit

---
+++ New Unity Project/Assets/FollowCamProbe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamProbe : MonoBehaviour
+{
+
+    ReflectionProbe probe;
+
+    private void Awake()
+    {
+        probe = GetComponent<ReflectionProbe>();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        probe.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y * -1, Camera.main.transform.position.z);
+        probe.RenderProbe();
+    }
+}
